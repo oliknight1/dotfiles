da7931fcfa8883e0a3f3d9d97b1461664f07c100
Oli Knight <olidknight@gmail.com>
2025-10-11 22:57:30 +0100

    actually fix lsp this time

--- private_dot_config/nvim/lua/oknight/plugins/lsp/lspconfig.lua
+++ private_dot_config/nvim/lua/oknight/plugins/lsp/lspconfig.lua
@@ -2,7 +2,10 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		lazy = false,
-		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+		},
 		keys = {
 			{
 				"K",
@@ -11,41 +14,56 @@ return {
 				end,
 				desc = "LSP signature help",
 			},
-			{
-				"gR",
-				"<cmd> Telescope lsp_references<CR>",
-				desc = "Show LSP references (Telescope)",
-			},
-			{
-				"gD",
-				"<Cmd>lua vim.lsp.buf.declaration()<CR>",
-				desc = "Go to declaration",
-			},
-			{
-				"gi",
-				"<Cmd>lua vim.lsp.buf.implementations()<CR>",
-				desc = "Go to implementations",
-			},
-			{
-				"gt",
-				"<Cmd>lua vim.lsp.buf.type_definitions()<CR>",
-				desc = "Go to type defintion",
-			},
-			{
-				"<leader>ca",
-				"<Cmd>lua vim.lsp.buf.code_action()<CR>",
-				desc = "Code action",
-			},
-			{
-				"<leader>rs",
-				"<Cmd>LSPRestart<CR>",
-				desc = "LSP Restart",
-			},
+			{ "gR", "<cmd> Telescope lsp_references<CR>", desc = "Show LSP references (Telescope)" },
+			{ "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", desc = "Go to declaration" },
+			{ "gi", "<Cmd>lua vim.lsp.buf.implementations()<CR>", desc = "Go to implementations" },
+			{ "gt", "<Cmd>lua vim.lsp.buf.type_definitions()<CR>", desc = "Go to type defintion" },
+			{ "<leader>ca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", desc = "Code action" },
+			{ "<leader>rs", "<Cmd>LSPRestart<CR>", desc = "LSP Restart" },
 		},
 		config = function()
 			vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 			vim.lsp.handlers["textDocument/signatureHelp"] =
 				vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+
+			local lspconfig = require("lspconfig")
+			local on_attach = function(client, bufnr) end
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+			local servers = {
+				lua_ls = {
+					settings = {
+						Lua = {
+							hint = { enable = true },
+							diagnostics = { globals = { "vim" } },
+							workspace = {
+								library = {
+									[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+									[vim.fn.stdpath("config") .. "/lua"] = true,
+								},
+							},
+						},
+					},
+				},
+				gopls = {
+					root_dir = require("lspconfig.util").root_pattern("go.work", "go.mod", ".git"),
+					settings = {
+						gopls = {
+							completeUnimported = true,
+							analyses = { unusedparams = true },
+							staticcheck = true,
+						},
+					},
+				},
+			}
+
+			for name, conf in pairs(servers) do
+				conf = vim.tbl_deep_extend("force", {
+					on_attach = on_attach,
+					capabilities = capabilities,
+				}, conf or {})
+				lspconfig[name].setup(conf)
+			end
 		end,
 	},
 }
--- private_dot_config/nvim/lua/oknight/plugins/lsp/mason.lua
+++ private_dot_config/nvim/lua/oknight/plugins/lsp/mason.lua
@@ -8,9 +8,7 @@ return {
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
 		local mason_tool_installer = require("mason-tool-installer")
-		local lspconfig = require("lspconfig")
 
-		-- Mason UI
 		mason.setup({
 			ui = {
 				icons = {
@@ -21,71 +19,21 @@ return {
 			},
 		})
 
-		-- Mason LSP
 		mason_lspconfig.setup({
 			ensure_installed = {
 				"gopls",
 				"lua_ls",
-				"ts_ls",
-				"html",
-				"cssls",
-				"graphql",
-				"emmet_ls",
-				"pyright",
-				"marksman",
 			},
-			automatic_installation = true,
+			automatic_installation = false,
 		})
 
-		-- Mason Tool Installer
 		mason_tool_installer.setup({
 			ensure_installed = {
-				"prettier",
 				"stylua",
-				"isort",
-				"black",
-				"ruff",
-				"eslint",
 				"staticcheck",
 				"goimports",
 			},
+			auto_update = true,
 		})
-
-		-- LSP defaults
-		local on_attach = function(client, bufnr) end
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
-
-		-- Manually setup servers (instead of setup_handlers)
-		local servers = {
-			lua_ls = {
-				settings = {
-					Lua = {
-						hint = { enable = true },
-						diagnostics = { globals = { "vim" } },
-						workspace = {
-							library = {
-								[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-								[vim.fn.stdpath("config") .. "/lua"] = true,
-							},
-						},
-					},
-				},
-			},
-			gopls = {
-				settings = {
-					gopls = {
-						completeUnimported = true,
-						analyses = { unusedparams = true },
-						staticcheck = true,
-					},
-				},
-			},
-		}
-
-		for name, config in pairs(servers) do
-			config.capabilities = capabilities
-			config.on_attach = on_attach
-			lspconfig[name].setup(config)
-		end
 	end,
 }
