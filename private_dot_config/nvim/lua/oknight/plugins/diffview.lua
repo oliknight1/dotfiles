b995fcfbca3ce235701c17bc320261741701b3c4
Oli Knight <olidknight@gmail.com>
2026-04-01 10:59:19 +0100

    add diffview

---
+++ private_dot_config/nvim/lua/oknight/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+	"sindrets/diffview.nvim",
+	keys = {
+		{ "<leader>dvo", "<cmd> DiffviewOpen<CR>", desc = "Diff View Open" },
+		{ "<leader>dvc", "<cmd> DiffviewClose<CR>", desc = "Diff View Close" },
+	},
+}
